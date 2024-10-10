a93dc3d46e6933c613eda83312d7a275ce3894e3
claudiopatti <claudiopatti1997@gmail.com>
2024-10-10 19:55:45 +0200

    aggiunta struttura base con prima classe movie con all interno delle variabili

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+
+class Movie {
+
+    public $title;         //string
+    public $genre;         //array-string
+    public $actor;         //array
+    public $language;      //array-string
+    public $director;      //string
+}
+
+$cesaroni = new Movie();
+
+var_dump($cesaroni);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP OOP 1</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
