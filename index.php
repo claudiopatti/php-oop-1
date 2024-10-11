286828757b9b8ea3963d4fff7105262ad32b6f12
claudiopatti <claudiopatti1997@gmail.com>
2024-10-11 09:32:45 +0200

    aggiunti 2 metodi

--- index.php
+++ index.php
@@ -2,19 +2,49 @@
 
 class Movie {
 
-    public $title;         //string
+    private $title;         //string
     public $director;      //string
     public $genre;         //array-string
+    private $year;         //string
     public $actor;         //array
     public $language;      //array-string
 
-    function __construct($title, $director, $genre) {
+    function __construct($title, $director, $genre, $year) {
         $this->title = $title;
         $this->director = $director;
         $this->genre = $genre;
+        $this->year = $year;
 
     }
-}
+
+    public function getTitle() {
+        return $this->title;
+
+    }
+
+    public function setTitle($title) {
+        if (is_string($title) && strlen($title) > 0 ) {
+            $this->title = $title;
+        }
+        else {
+            'error';
+        }
+    }
+
+    public function getYear() {
+        return $this->year;
+
+    }
+
+    public function setYear($year) {
+        if (is_numeric($year)) {
+            $this->year = $year;
+        }
+        else {
+            'error';
+        }
+    }
+};
 
 
 $director = 'Francesco Vicario';
@@ -24,7 +54,7 @@ $genre = [
     'romantic'
 ];
 
-$cesaroni = new Movie('i cesaroni', $director, $genre);
+$cesaroni = new Movie('i cesaroni', $director, $genre, 2006);
 
 var_dump($cesaroni);
 
